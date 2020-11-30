df47478f0b8ffc64c64c311775a97824fbade044
Arkin Modi <arkinmodi@gmail.com>
2020-11-30 17:43:18 -0500

    Added score & collision detection

--- script.js
+++ script.js
@@ -44,6 +44,18 @@ window.onload = function () {
         this.y = canvas.height - this.radius;
       }
     }
+
+    getX() {
+      return this.x;
+    }
+
+    getY() {
+      return this.y;
+    }
+
+    getRadius() {
+      return this.radius;
+    }
   }
 
   class Square {
@@ -60,43 +72,85 @@ window.onload = function () {
       context.rect(this.x, this.y, this.length, this.length);
       context.fill();
     }
+
+    getX() {
+      return this.x;
+    }
+
+    getY() {
+      return this.y;
+    }
+
+    getLength() {
+      return this.length;
+    }
   }
 
-  // Initialize the game
-  let score = 0;
-  let playerRadius = 25;
-  let holeLength = 50;
-  let speedMultiplier = 1;
+  function newGame() {
+    // Number between 25 and (canvas width or height - 25)
+    // Makes sure player starts inbounds
+    let startBallX =
+      Math.floor(Math.random() * (canvas.width - playerRadius * 2)) +
+      playerRadius;
+    let startBallY =
+      Math.floor(Math.random() * (canvas.height - playerRadius * 2)) +
+      playerRadius;
 
-  // Current device orientation
-  let beta = 0;
-  let gamma = 0;
+    // Start hole at ball and keep changing until hole is not on ball
+    // Square (x, y) is at top left corner
+    let startHoleX = startBallX;
+    let startHoleY = startBallY;
+    while (
+      Math.abs(startHoleX - startBallX) < holeLength + playerRadius &&
+      Math.abs(startHoleY - startBallY) < holeLength + playerRadius
+    ) {
+      // Number between 0 and (canvas width or height - holeLength)
+      startHoleX = Math.floor(Math.random() * (canvas.width - holeLength + 1));
+      startHoleY = Math.floor(Math.random() * (canvas.height - holeLength + 1));
+    }
 
-  // Number between 25 and (canvas width or height - 25)
-  // Makes sure player starts inbounds
-  let startBallX =
-    Math.floor(Math.random() * (canvas.width - playerRadius * 2)) +
-    playerRadius;
-  let startBallY =
-    Math.floor(Math.random() * (canvas.height - playerRadius * 2)) +
-    playerRadius;
-
-  // Start hole at ball and keep changing until hole is not on ball
-  // Square (x, y) is at top left corner
-  let startHoleX = startBallX;
-  let startHoleY = startBallY;
-  while (
-    Math.abs(startHoleX - startBallX) < holeLength + playerRadius &&
-    Math.abs(startHoleY - startBallY) < holeLength + playerRadius
-  ) {
-    // Number between 0 and (canvas width or height - holeLength)
-    startHoleX = Math.floor(Math.random() * (canvas.width - holeLength + 1));
-    startHoleY = Math.floor(Math.random() * (canvas.height - holeLength + 1));
+    // Game objects
+    player = new Ball(startBallX, startBallY, playerRadius, "green");
+    hole = new Square(startHoleX, startHoleY, holeLength, "black");
   }
 
-  // Game objects
-  let player = new Ball(startBallX, startBallY, playerRadius, "green");
-  let hole = new Square(startHoleX, startHoleY, holeLength, "black");
+  function isGoal(player, hole) {
+    /*  
+        LOGIC
+        1.  Check which sides (left or right & top or bottom) of the square the 
+            circle is closest to. (e.g., if circle is above, closest is top)
+
+        2.  Use Pythagorean Theorem to find the distance between center of 
+            circle and the side of the square.
+
+        3.  If this distance is less than the radius of circle, there is a 
+            collision.
+    */
+
+    let x = player.getX();
+    let y = player.getY();
+
+    if (player.getX() < hole.getX()) {
+      x = hole.getX(); // left side
+    } else if (player.getX() > hole.getX() + hole.getLength()) {
+      x = hole.getX() + hole.getLength(); // right side
+    }
+
+    if (player.getY() < hole.getY()) {
+      y = hole.getY(); // top side
+    } else if (player.getY() > hole.getY() + hole.getLength()) {
+      y = hole.getY() + hole.getLength(); // bottom side
+    }
+
+    let distX = player.getX() - x;
+    let distY = player.getY() - y;
+    let distance = Math.sqrt(distX * distX + distY * distY);
+
+    if (distance <= player.getRadius()) {
+      return true;
+    }
+    return false;
+  }
 
   function gameLogic() {
     // Update position based on device orientation
@@ -105,14 +159,35 @@ window.onload = function () {
       gamma = event.gamma;
     };
     player.move(gamma * speedMultiplier, beta * speedMultiplier);
+
+    if (isGoal(player, hole)) {
+      score++;
+      newGame();
+      console.log(score);
+    }
   }
 
   function updateFrame() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     hole.draw();
     player.draw();
+    document.getElementById("score").innerHTML = "Score: " + score;
   }
 
+  // Current device orientation
+  let beta = 0;
+  let gamma = 0;
+
+  // Initialize the game variables
+  let score = 0;
+  let playerRadius = 25;
+  let holeLength = 50;
+  let speedMultiplier = 1;
+  let player;
+  let hole;
+
+  // Start Game
+  newGame();
   setInterval(function () {
     gameLogic();
     updateFrame();
